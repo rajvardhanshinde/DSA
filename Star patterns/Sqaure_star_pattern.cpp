9384cfb6133cc7c42d93b376e076932d8ee48db7
Rajvardhan Shinde <shinderajvardhan2051@gmail.com>
2024-02-08 16:42:16 +0530

    square star pattern with n no of rows and columns

--- Star patterns/Sqaure_star_pattern.cpp
+++ Star patterns/Sqaure_star_pattern.cpp
@@ -10,3 +10,28 @@ int main(){
 		cout<<endl;
 	}
 }   
+
+
+
+
+// [ In the below code nr and nc are no_of_rows and no_of_columns.
+// You can print no of rows and columns star pattern by giving you required values to nr and nc. ]
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 	int nr,nc,row,col;
+// 	cout<<"Enter no of rows :"<<endl;
+// 	cin>>nr;
+// 	cout<<"Enter no of coloumns :"<<endl;
+// 	cin>>nc;
+
+// 	for(row=0;row<nr;row=row+1){
+// 		for(col=0;col<nc;col++){
+// 			cout<<"* ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }   
