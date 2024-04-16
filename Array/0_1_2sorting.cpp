a7cc77cccc787c297eb03c09c862645bea31e673
Rajvardhan shinde <110803326+rajvardhanshinde@users.noreply.github.com>
2024-04-16 11:57:48 +0530

    012 sort

--- Array/0_1_2sorting.cpp
+++ Array/0_1_2sorting.cpp
@@ -1,47 +1,43 @@
-#include <iostream>
-// #include<vector>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int arr[] = {1, 2, 0, 2, 1, 0, 0, 2};
-    int sizea = 8;
-    int count0 = 0;
-    int count1 = 0;
-    int count2 = 0;
-
-    // vector<int> newarr;
-    for (int i = 0; i < sizea; i++)
-    {
-        if (arr[i] == 0)
-        {
-            count0++;
-           
-            
-        }
-        else if (arr[i] == 1)
-        {
-            count1++;
-           
-        }
-        else
-        {
-            count2++;
-            
-        }
-        
-      
-    }
-     for (int j = 0; j < count0; j++)
-            {
-                arr[k+1]=0;
-            }
-    //   cout<<count0;
-        
-
-    for (int i = 0; i <sizea; i++)
-    {
-        cout << " " << arr[i];
-    }
-
-    return 0;
+int main(){
+	int arr[]={1,2,0,0,2,1,1,2};
+	int sizea=8;
+	int k=0;
+	int count0=0;
+	int count1=0;
+	int count2=0;
+	for(int i=0;i<sizea;i++)
+	{
+		if(arr[i]==0){
+		 count0++; 
+		 } 
+		else if(arr[i]==1)
+		 { 
+			 count1++; 
+			 }
+		else if(arr[i]==2){
+			count2++;
+			}
+		}
+		while(count0){
+			arr[k]=0;
+			k++;
+			count0--;
+			}
+			while(count1){
+			arr[k]=1;
+			k++;
+			count1--;
+			}
+			while(count2){
+			arr[k]=2;
+			k++;
+			count2--;
+			}
+			for(int i=0;i<sizea;i++)
+			{
+				cout<<arr[i];
+				}
+	return 0;
 }
