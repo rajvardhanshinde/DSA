26d4044ba230e070e062de2ef2dcacf8d70874f4
Rajvardhan shinde <110803326+rajvardhanshinde@users.noreply.github.com>
2024-04-16 12:36:06 +0530

    Update Negative_left_side.cpp

--- Array/Negative_left_side.cpp
+++ Array/Negative_left_side.cpp
@@ -1,35 +1,32 @@
 
-error
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> arr{2,3,-12,76,-23,-21,1,-90};
-    int start=0;
-    int i=0;
-    int end=arr.size()-1;
-
-    for (int i = 0; i <= arr.size(); i++)
-    {
-        if (arr[i]<0)
-        {
-            swap(arr[i],start);
-            i++;
-            start++;
-        }
-        if (arr[i]>0)
-        {
-            swap(arr[i],end);
-        
-            start--;
-        }
-        
-    }
-    for (int i = 0; i < arr.size(); i++)
-    {
-         cout<<arr[i];
-    }
-    
+    vector<int> arr{2,3,-1,7,-2,-7};
+	int start=0;
+	int end=arr.size()-1;
+	int z=end;
+	int i=0;
+	while(start<end)
+	{
+		if(arr[i]<0)
+		{
+			swap(arr[i],arr[start]);
+			start++;
+			i++;
+			
+			}
+		else 
+		{
+			swap(arr[i],arr[end]);
+			end--;
+			
+			}
+		}
+	for(i=0;i<z+1;i++){
+		cout<<arr[i]<<" ";
+		}
    
     return 0;
     
